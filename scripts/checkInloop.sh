afb1050cca15b4843903d041b1da7e2b2453b680
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2017-06-03 21:14:52 +0200

    (2017-06-03) change dotFiles

--- scripts/checkInloop.sh
+++ scripts/checkInloop.sh
@@ -2,9 +2,14 @@
 
 git fetch origin
 git rebase origin/master
-echo "Make Docker"
+echo "Make Docker..."
 make dockerimage > /dev/null
-echo "Run Docker"
-python3 run_docker.py 2>&1| grep "Failed tasks:" | awk '{print "Docker: " $1}'
-echo "Run Gradle"
+echo "Run Docker..."
+DOCKER=$(python3 run_docker.py BasicLibrary 2>&1| grep "Failed tasks:")
+if [ -n "$DOCKER" ]; then
+    echo "Docker: BUILD FAILED"
+else
+    echo "Docker: BUILD SUCCESSFUL"
+fi
+echo "Run Gradle..."
 ./gradlew clean test 2>&1| grep "BUILD" | awk '{print "Gradle: " $1" "$2}'
