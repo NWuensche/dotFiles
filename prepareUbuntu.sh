e4c9f576c599570c7d2723b87db98c112fbdb20f
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2017-10-22 14:03:54 +0200

    (22-10-2017) change dotFiles

--- prepareUbuntu.sh
+++ prepareUbuntu.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
+echo "First install Android-Studio!"
+echo "Also Add saveFolder"
 sudo apt update
 sudo apt upgrade
-sudo apt install default-jdk android-studio chromium-browser pwgen xclip vim python3 maven redshift steam calibre htop i3 git vlc curl vifm zsh terminator gparted ffmpeg gimp xss-lock xautolock phantomjs virtualbox youtube-dl trash-cli scrot udiskie xbacklight
+sudo apt install default-jdk android-studio chromium-browser pwgen xclip vim python3 maven redshift steam calibre htop i3 git vlc curl vifm zsh terminator gparted ffmpeg gimp xss-lock xautolock phantomjs virtualbox youtube-dl trash-cli scrot udiskie xbacklight feh
 sudo apt autoremove firefox
+
+mv dotFiles .dotFiles
 
 #Just for super-user-spark
 curl https://nixos.org/nix/install | sh
 . /home/nwuensche/.nix-profile/etc/profile.d/nix.sh
 nix-env --install super-user-spark
-
-git clone https://github.com/NWuensche/dotFiles
-mv dotFiles .dotFiles
 ~/.nix-profile/bin/spark -r deploy ~/.dotFiles/dotFiles.sus
+~/.nix-profile/bin/spark -r deploy ~/saveFolder/saveFolder.sus
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
@@ -24,6 +26,9 @@ cd fonts
 ./install.sh
 cd ..
 rm -rf fonts
-sudo ln -s ~/android-studio/bin/studio.sh /bin/a
 
-echo "Don't forget Android-Studio!"
+ln -s ~/android-studio/bin/studio.sh ~/bin/
+
+/bin/mv ~/.dotFiles/terminalStuff/agnoster.zsh-theme .oh-my-zsh/themes/agnoster.zsh-theme 
+
+
