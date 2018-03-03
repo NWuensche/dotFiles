d772b27aa1b278d11bbe4a2a27af5bc016c4a284
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2018-03-03 10:56:43 +0100

    (03-03-2018) change dotFiles

--- scripts/jexam.sh
+++ scripts/jexam.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-length=$(curl https://twitter.com/jExamUpdates | grep "Beep boop" | wc -l ); 
-if [ $length -gt 17 ]
+#length=$(curl https://twitter.com/jExamUpdates | grep "Beep boop" | wc -l ); 
+length=$(curl http://feeds.feedburner.com/jExam\? | awk -F'description' '{print $3}' | awk -F'Die Modul' '{print $1}' | tr ' ' '\n' |grep INF-B -c)
+if [ $length -gt 2 ]
 then 
-    notify-send "JEXAM" "Neu"
+    notify-send -t 1000000 "JEXAM" "Neu"
 fi
 
--- scripts/podgetNotify.sh
+++ scripts/podgetNotify.sh
@@ -3,5 +3,5 @@ t=$(podget  2>&1 | grep "Downloading" | grep -v "feed index" | wc -l)
 
 if [ $t -gt 0 ]
 then
-    notify-send "Podget" "New Podcast"
+    notify-send -t 1000000 "Podget" "New Podcast"
 fi
