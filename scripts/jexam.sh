e7c699a28aa2531ca7828d116b4d6db4158769f7
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2018-03-25 16:12:53 +0200

    (25-03-2018) change dotFiles

--- scripts/jexam.sh
+++ scripts/jexam.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #length=$(curl https://twitter.com/jExamUpdates | grep "Beep boop" | wc -l ); 
 length=$(curl http://feeds.feedburner.com/jExam\? | awk -F'description' '{print $3}' | awk -F'Die Modul' '{print $1}' | tr ' ' '\n' |grep INF-B -c)
-if [ $length -gt 4 ]
+if [ $length -gt 5 ]
 then 
     notify-send -t 10000000 "JEXAM" "Neu"
 fi
