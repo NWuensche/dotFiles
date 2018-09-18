f48824bad883faebf656eb5654b9db8aee719a59
nwuensche <Wuensche.Niklas@gmail.com>
2018-09-18 19:46:47 +0200

    (18-09-2018) change dotFiles

--- prepareUbuntu.sh
+++ prepareUbuntu.sh
@@ -30,7 +30,7 @@ sudo systemctl enable lightdm.service
 
 # Remove vim to install gvim for better clipboard support
 sudo trizen -R vim --noconfirm
-sudo trizen -S jdk10-openjdk  tmux pwgen xclip gvim python3 maven redshift steam calibre htop git vlc curl neomutt wget vifm zsh terminator gparted ffmpeg gimp xss-lock xautolock virtualbox youtube-dl trash-cli scrot udiskie ntfs-3g feh chromium texlive-most biber android-file-transfer wine unrar mps-youtube arandr pdfgrep kdenlive vim-spell-de vim-spell-en cronie calcurse lynx w3m alsa-utils pulseaudio virtualbox virtualbox-host-modules-arch virtualbox-guest-iso libreoffice-fresh libreoffice-fresh-de  ttf-liberation  openssh pango evince notification-daemon imapfilter urlview android-studio intellij-idea-ultimate-edition intellij-idea-ultimate-edition-jre bc texlive-localmanager-git mplayer irssi tcsh cups expect --noconfirm
+sudo trizen -S jdk10-openjdk  tmux pwgen xclip gvim python3 maven redshift steam calibre htop git vlc curl neomutt wget vifm zsh terminator gparted ffmpeg gimp xss-lock xautolock virtualbox youtube-dl trash-cli scrot udiskie ntfs-3g feh chromium texlive-most biber android-file-transfer wine unrar mps-youtube arandr pdfgrep kdenlive vim-spell-de vim-spell-en cronie calcurse lynx w3m alsa-utils pulseaudio virtualbox virtualbox-host-modules-arch virtualbox-guest-iso libreoffice-fresh libreoffice-fresh-de  ttf-liberation  openssh pango evince notification-daemon imapfilter urlview android-studio intellij-idea-ultimate-edition intellij-idea-ultimate-edition-jre bc texlive-localmanager-git mplayer irssi tcsh cups xdotool expect --noconfirm
 
 #User is allowed to change audio
 sudo usermod -aG audio nwuensche
---
+++ scripts/fullscreenLock.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#If fullscreen, set cursor to corner to stop xautolock from activating
+while true; do
+    WINDOW=$(echo $(xwininfo -id $(xdotool getactivewindow) -stats | \
+                    egrep '(Width|Height):' | \
+                    awk '{print $NF}') | \
+             sed -e 's/ /x/')
+    SCREEN=$(xdpyinfo | grep -m1 dimensions | awk '{print $2}')
+    if [ "$WINDOW" = "$SCREEN" ]; then
+        xdotool mousemove 0 0
+    fi
+    sleep 580
+done
