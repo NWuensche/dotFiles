676271129a6768f1b9ddca55c245cf0ddad6d933
nwuensche <Wuensche.Niklas@gmail.com>
2018-09-21 20:47:05 +0200

    (21-09-2018) change dotFiles

--- scripts/fullscreenLock.sh
+++ scripts/fullscreenLock.sh
@@ -2,6 +2,7 @@
 
 #If fullscreen, set cursor to corner to stop xautolock from activating
 while true; do
+    sleep 580
     WINDOW=$(echo $(xwininfo -id $(xdotool getactivewindow) -stats | \
                     egrep '(Width|Height):' | \
                     awk '{print $NF}') | \
@@ -10,5 +11,4 @@ while true; do
     if [ "$WINDOW" = "$SCREEN" ]; then
         xdotool mousemove 0 0
     fi
-    sleep 580
 done
