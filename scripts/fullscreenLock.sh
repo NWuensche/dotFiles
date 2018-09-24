65ed9f582f98ff71fbff24b3ae680aff1b6e4e45
nwuensche <Wuensche.Niklas@gmail.com>
2018-09-24 15:11:32 +0200

    (24-09-2018) change dotFiles

--- scripts/fullscreenLock.sh
+++ scripts/fullscreenLock.sh
@@ -2,13 +2,20 @@
 
 #If fullscreen, set cursor to corner to stop xautolock from activating
 while true; do
-    sleep 580
+    sleep 5
+    #Just do this for Chromium
+    NAMEWINDOW=$(echo $(xwininfo -id $(xdotool getactivewindow)) | sed -n -r '/(youtube|ted).*Chromium/Ip')
     WINDOW=$(echo $(xwininfo -id $(xdotool getactivewindow) -stats | \
                     egrep '(Width|Height):' | \
                     awk '{print $NF}') | \
              sed -e 's/ /x/')
     SCREEN=$(xdpyinfo | grep -m1 dimensions | awk '{print $2}')
     if [ "$WINDOW" = "$SCREEN" ]; then
-        xdotool mousemove 0 0
+        if [ "$NAMEWINDOW" != "" ]; then
+            X=$( xdotool getmouselocation | sed -n 's/.*x:\([[:alnum:]]*\).*/\1/p' )
+            Y=$( xdotool getmouselocation | sed -n 's/.*y:\([[:alnum:]]*\).*/\1/p' )
+            xdotool mousemove 0 0
+            xdotool mousemove `echo $X` `echo $Y`
+        fi
     fi
 done
