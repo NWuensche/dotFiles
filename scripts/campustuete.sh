96db23064249c06c796dfdfb09c604e31e3dda21
nwuensche <Wuensche.Niklas@gmail.com>
2019-04-11 09:41:17 +0200

    (11-04-2019) change dotFiles

--- scripts/campustuete.sh
+++ scripts/campustuete.sh
@@ -1,7 +1,6 @@
 SITE=$(curl -s https://de-de.facebook.com/campustuete/ )
 NUM_BERLIN=$( echo "$SITE" | tr ' ' '\n' | tr ',' '\n' | tr '<' '\n' | grep Berlin | wc -l )
-echo "$NUM_BERLIN"
 
-if (( $NUM_BERLIN != 1 )); then
-  notify-send "Look at Facebook Campus Tüte"
+if (( $NUM_BERLIN != 3 && $NUM_BERLIN != 0 )); then
+  notify-send "Look at Facebook Campus Tüte - Num: $NUM_BERLIN"
 fi
