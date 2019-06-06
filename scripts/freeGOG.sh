0e5b467dcce53a5462b76a6128e0761b73f2d71c
nwuensche <Wuensche.Niklas@gmail.com>
2019-06-06 19:45:44 +0200

    (06-06-2019) change dotFiles

--- scripts/freeGOG.sh
+++ scripts/freeGOG.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e
+ping -W 2 8.8.8.8 -c 2 #Check Connection
 FREE=$(curl --connect-timeout 2 -s https://www.freegamekeys.com/gog/ --compressed | sed -n '/Free/p' | head -n 2 | tail -n 1 | sed -n '/Free+Obduction/p' ) #If DISTRAINT isn't the latest free game anymore, then notify me
 if [[ "$FREE" == "" ]] ; then
   notify-send "GOG Free Game";
