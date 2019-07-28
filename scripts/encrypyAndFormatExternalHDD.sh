3f671b3b4cec9cc1e3f8d62d2eab7e1c800425d1
nwuensche <Wuensche.Niklas@gmail.com>
2019-07-28 11:47:55 +0200

    (28-07-2019) change dotFiles

---
+++ scripts/encrypyAndFormatExternalHDD.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+sed -e 's/\s*\([+0-9a-zA-Z]*\).*/\1/' << EOF | sudo fdisk /dev/sdb
+  o # clear current table (root)
+  n # new partition (LVM - Rest)
+  p # primary
+  1 # number 1
+    # default - start at beginning of disk
+    # default - till the end of disk
+  w # write
+EOF
+
+#If error here, delete partition with gparted first and unmount HDD
+sudo cryptsetup -y -v luksFormat --type luks2 /dev/sdb1
+
+sudo cryptsetup open /dev/sdb1 exthdd3
+
+echo Y | sudo mkfs.ext4 /dev/mapper/exthdd3
+
+echo "Also do last line script after remount to make HDD writeable"
+#sudo chown -R $USER /run/media/$USER/20118cj7-33b1-4f38-bbf5-94a58dee2751
+
+
+
