e90672ee8e3a42c16f68dca60ef0418da1767a11
nwuensche <Wuensche.Niklas@gmail.com>
2019-09-26 18:11:48 +0200

    (26-09-2019) change dotFiles

--- scripts/freeGOG.sh
+++ scripts/freeGOG.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 ping -W 2 8.8.8.8 -c 2 #Check Connection
-FREE=$(curl --connect-timeout 2 -s https://www.freegamekeys.com/gog/ --compressed | sed -n '/Check Giveaway/p' | head -n 1 | sed -n '/Toonstruck/p' ) #If DISTRAINT isn't the latest free game anymore, then notify me
+FREE=$(curl --connect-timeout 2 -s https://www.freegamekeys.com/gog/ --compressed | sed -n '/Check Giveaway/p' | head -n 1 | sed -n '/Freespace/p' ) #If DISTRAINT isn't the latest free game anymore, then notify me
 if [[ "$FREE" == "" ]] ; then
   notify-send "GOG Free Game";
 fi
