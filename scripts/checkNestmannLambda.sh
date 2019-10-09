e15e0f1d2d64846bd2ec951d96f73f23179fe641
nwuensche <Wuensche.Niklas@gmail.com>
2019-10-09 08:50:10 +0200

    (09-10-2019) change dotFiles

--- scripts/checkNestmannLambda.sh
+++ scripts/checkNestmannLambda.sh
@@ -2,7 +2,7 @@
 set -e
 ping -q -W 2 8.8.8.8 -c 2 #Check Connection
 SITE=$(curl --connect-timeout 2 -s https://www.mtv.tu-berlin.de/menue/lehre/module/latys/ )
-LASTUPDATE=$(echo "$SITE" | sed -n '/Zuletzt aktualisiert:&nbsp;23\.05\.19/p' )
+LASTUPDATE=$(echo "$SITE" | sed -n '/Zuletzt aktualisiert:&nbsp;02\.10\.19/p' )
 if [[ "$LASTUPDATE" == "" ]] ; then
   notify-send "Nestmann Lambda new Infos";
 fi
