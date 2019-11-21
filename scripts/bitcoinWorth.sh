ce8a989192851f2cc716903957bc0a559d4f6c4a
nwuensche <Wuensche.Niklas@gmail.com>
2019-11-21 19:34:23 +0100

    (21-11-2019) change dotFiles

--- scripts/bitcoinWorth.sh
+++ scripts/bitcoinWorth.sh
@@ -1,5 +1,5 @@
 WHOLE_EUROS=$( curl https://www.bitcoin.de/ -s | sed -n 's/.*Aktueller Bitcoin Kurs:.*>\(.*\)€.*/\1/p' | sed 's/\.//' | sed 's/,.*//')
 
-if (( $WHOLE_EUROS > 100000 )) ; then
-  notify-send "Bitcoin Worth now over 100000€"
+if (( $WHOLE_EUROS > 10000 )) ; then
+  notify-send "Bitcoin Worth now over 10000€"
 fi
