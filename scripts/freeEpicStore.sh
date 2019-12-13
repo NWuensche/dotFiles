57cdc92a95ffe7f9d6002255eca118a9e0c3a285
nwuensche <Wuensche.Niklas@gmail.com>
2019-12-13 07:57:01 +0100

    (13-12-2019) change dotFiles

--- scripts/freeEpicStore.sh
+++ scripts/freeEpicStore.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 ping -W 2 8.8.8.8 -c 2 #Check Connection
-FREE=$(curl --connect-timeout 2 -s https://www.freegamekeys.com/epic-store/ --compressed | sed -n '/Check Giveaway/p' | head -n 1 | sed -n '/Jotun/p' ) #If DISTRAINT isn't the latest free game anymore, then notify me
+FREE=$(curl --connect-timeout 2 -s https://www.freegamekeys.com/epic-store/ --compressed | sed -n '/Check Giveaway/p' | head -n 1 | sed -n '/Escapists/p' ) #If DISTRAINT isn't the latest free game anymore, then notify me
 if [[ "$FREE" == "" ]] ; then
   notify-send "Epic Store Free Game";
 fi
