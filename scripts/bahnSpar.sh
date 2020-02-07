e306a019aa5ee0f5df42f14180e09a6e50866211
nwuensche <Wuensche.Niklas@gmail.com>
2020-02-07 15:27:20 +0100

    (07-02-2020) change dotFiles

--- scripts/bahnSpar.sh
+++ scripts/bahnSpar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 SITE_TIME=$(curl -s https://www.zugreiseblog.de/bahn-gutschein/ | sed -n 's/.*Aktualisiert: <time class=updated datetime="\([^"]*\).*/\1/p')
-LAST_TIME="2019-12-31 19:50:10"
+LAST_TIME="2020-01-31 12:27:49"
 
 if [[ "$SITE_TIME" != "$LAST_TIME" ]]; then
   notify-send "Bahn Rabatt New Stuff"
