f9282ddf434ce91ec548589f92103eff2f787906
nwuensche <Wuensche.Niklas@gmail.com>
2020-02-29 20:22:31 +0100

    (29-02-2020) change dotFiles

--- rules/keyboardscript.sh
+++ rules/keyboardscript.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 export DISPLAY=":0.0"
-#export XAUTHORITY="/home/nwuensche/.Xauthority"
 #& here (for some reason) important, otherwise this doesn't get applied
 /usr/bin/setxkbmap eu -option caps:escape &
--- scripts/freeGames.sh
+++ scripts/freeGames.sh
@@ -1,5 +1,5 @@
 freeEpic() {
-  FREE=$(curl --connect-timeout 2 -s https://www.freegamekeys.com/epic-games-store/ --compressed | sed -n '/Check Giveaway/p' | head -n 1 | sed -n '/Kingdom Come/p' ) #If DISTRAINT isn't the latest free game anymore, then notify me
+  FREE=$(curl --connect-timeout 2 -s https://www.freegamekeys.com/epic-games-store/ --compressed | sed -n '/Check Giveaway/p' | head -n 1 | sed -n '/Inner/p' ) #If DISTRAINT isn't the latest free game anymore, then notify me
   if [[ "$FREE" == "" ]] ; then
     notify-send "Epic Store Free Game";
   fi
@@ -28,9 +28,17 @@ freeOrigin() {
 
 }
 
+freeSteam() {
+  FREE=$(curl --connect-timeout 2 -s https://steamcommunity.com/groups/freegamesfinders/rss/  | sed -n '/title.*from Steam/Ip' | head -n 1 | sed -n '/Black Des/p' )
+  if [[ "$FREE" == "" ]] ; then
+    notify-send "Steam Free Game";
+  fi
+}
+
 ping -q -W 2 8.8.8.8 -c 2 #Check Connection
 
 freeEpic
 freeGOG
 freeHB
 freeOrigin
+freeSteam
