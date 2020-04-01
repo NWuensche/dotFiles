460adadd460738d7fc924dee618b8852ecd53d9f
nwuensche <Wuensche.Niklas@gmail.com>
2020-04-01 11:38:35 +0200

    (01-04-2020) change dotFiles

--- scripts/freeGames.sh
+++ scripts/freeGames.sh
@@ -12,6 +12,13 @@ freeGOG() {
   fi
 }
 
+freeUPlayUbisoft() {
+  FREE=$(curl --connect-timeout 2 -s https://www.freegamekeys.com/uplay/ --compressed | sed -n '/Check Giveaway/p' | head -n 1 | sed -n '/Rayman Legends/p' ) #If DISTRAINT isn't the latest free game anymore, then notify me
+  if [[ "$FREE" == "" ]] ; then
+    notify-send "UPlay Free Game";
+  fi
+}
+
 freeHB() {
   FREE=$(curl --connect-timeout 2 -s https://www.humblebundle.com/store | grep -i "<p.*Free.*time")
 
@@ -29,7 +36,7 @@ freeOrigin() {
 }
 
 freeSteam() {
-  FREE=$(curl --connect-timeout 2 -s https://steamcommunity.com/groups/freegamesfinders/rss/  | sed -n '/title.*from Steam/Ip' | head -n 1 | sed -n '/Barro/p' )
+  FREE=$(curl --connect-timeout 2 -s https://steamcommunity.com/groups/freegamesfinders/rss/  | sed -n '/title.*from Steam/Ip' | head -n 1 | sed -n '/One Drop Bot/p' )
   if [[ "$FREE" == "" ]] ; then
     notify-send "Steam Free Game";
   fi
@@ -39,6 +46,7 @@ ping -q -W 2 8.8.8.8 -c 2 #Check Connection
 
 freeEpic
 freeGOG
+freeUPlayUbisoft
 freeHB
 freeOrigin
 freeSteam
