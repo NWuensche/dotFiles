be74a7549325ad003365b7d3b5c27a7c89d8f2dd
nwuensche <gh@nwuensche.com>
2022-04-14 17:04:59 +0200

    (14-04-2022) change dotFiles

--- scripts/bahnSpar.sh
+++ scripts/bahnSpar.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
+LIST='Super Sparpreis Aktion:
+BahnCard 25 zum Jubiläumspreis:
+Freifahrten, Gratis-Upgrades &amp; kostenlose BahnCard
+Für alle unter 27:
+25% Rabatt auf ICE-Tickets:
+Flixtrain-Tickets statt
+Als Gruppe ab 9,90€ im ICE fahren
+Sparpreis Europa ab 13,90€
+Bahn-Shop: Alles rund um die Bahn'
+
 set -e # to stop on failing ping
 ping -q -W 2 8.8.8.8 -c 2 #Check Connection
 
-SITE_TIME=$(curl -s --head https://www.bahndampf.de/angebote | sed -n 's/last-modified: \(.*\)./\1/p' ) #Ends on <C-M> for some reason
-LAST_TIME="Tue, 29 Mar 2022 16:20:31 GMT"
+#perl does replace unicode space with normal space, last sed remove trailing space
+NEWLIST=$(curl -s https://www.bahndampf.de/angebote |  sed -n 's/.*<td width="20%"><strong>\([^<]*\)\($\|<.*\)/\1/p' | perl -CSDA -plE 's/\s/ /g' | sed -e 's/[[:space:]]*$//')
 
-if [[ "$SITE_TIME" != "$LAST_TIME" ]]; then
+if [[ "$LIST" != "$NEWLIST" ]]; then
   notify-send "Bahn Rabatt New Stuff"
-  echo "$SITE_TIME"
+  echo "$NEWLIST"
 fi
